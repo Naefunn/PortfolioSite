52db6a81cf450d5cd37b842cd2c635bf530572db
Naefunn <nathan.d.foster1@gmail.com>
2022-11-09 19:10:47 +0000

    slider bug

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -1,10 +1,66 @@
 import React from "react";
 import './project.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import py1 from '../../assets/images/pyPro1.png';
+import py2 from '../../assets/images/pyPro2.png';
+import py3 from '../../assets/images/pyPro3.png';
+
 
 const Project = () => {
     
     return (
-        <div>This will be the project page</div>
+        <div className="projects">
+            <span>My Project</span>
+            <span>Portfolio</span>
+
+
+            <Swiper
+            spaceBetween={30}
+            slidesPerView={3}
+            grabCursor={true}
+            className="projects-slider"
+            >
+               <SwiperSlide>
+                    <img src={py1} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={py2} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={py3} alt=""/>
+                </SwiperSlide> 
+            </Swiper>
+{/* 
+            <Swiper className="js-project">
+               <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+            </Swiper> */}
+
+            {/* <Swiper className="dapp-project">
+               <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+
+                <SwiperSlide>
+                    <img src={} alt=""/>
+                </SwiperSlide> 
+            </Swiper> */}
+        </div>
     )
 }
 
